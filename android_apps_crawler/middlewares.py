2b853160771ea67ec45c07c7bd085e29aca67f86
mssun <robertsms@gmail.com>
2012-12-18 18:53:22 +0800

    add proxy

--- android_apps_crawler/middlewares.py
+++ android_apps_crawler/middlewares.py
@@ -2,6 +2,8 @@ from scrapy.exceptions import IgnoreRequest
 
 class DownloaderMiddleware(object):
     def process_request(self, request, spider):
+        request.meta["proxy"] = "http://proxy.cse.cuhk.edu.hk:8000"
         if request.url[-3:] in ["apk", "png"]:
             print "Ignore request!"
             raise IgnoreRequest
+
