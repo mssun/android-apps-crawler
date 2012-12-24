93aed4b653da1174bf75e3bc62eaff67c465bb5b
mssun <robertsms@gmail.com>
2012-12-24 20:04:50 +0800

    bug fixed

--- android_apps_crawler/middlewares.py
+++ android_apps_crawler/middlewares.py
@@ -3,7 +3,7 @@ from scrapy.exceptions import IgnoreRequest
 class DownloaderMiddleware(object):
     def process_request(self, request, spider):
         request.meta["proxy"] = spider.settings['PROXIES']['http']
-        if request.url[-3:] in ["apk", "png", "jpg", "exe", "doc",
+        if request.url[-3:].lower() in ["apk", "png", "jpg", "exe", "doc",
                 "zip"]:
             print "Ignore request!"
             raise IgnoreRequest
